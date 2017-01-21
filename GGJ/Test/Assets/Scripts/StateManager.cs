ed4ff61dd1aca15a9f3cfd0b1e775e141833b048
TrevorNewell <u0774750@utah.edu>
2017-01-21 13:22:08 -0700

    Menu Shtuff

---
+++ GGJ/Test/Assets/Scripts/StateManager.cs
@@ -0,0 +1,143 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+using System;
+using UnityEngine.UI;
+
+public class StateManager : MonoBehaviour
+{
+    public static StateManager instance;
+    public enum State
+    {
+        PressStart, MainMenu, InGame, Pause, Credits
+    };
+
+    public Font ourFont;
+
+    private State theState;
+
+    public GameObject[] screens;
+    private GameObject mainMenu;
+    private GameObject pressStartMenu;
+    private GameObject pauseMenu;
+
+    public State TheState
+    {
+        get { return theState; }
+        set
+        {
+            theState = value;
+
+            Debug.Log("State: " + theState.ToString());
+
+            if (theState == State.PressStart)
+            {
+                DeactivateAll();
+                //pressStartMenu.SetActive(true);
+            }
+            else if (theState == State.MainMenu)
+            {
+                DeactivateAll();
+                mainMenu.SetActive(true);
+            }
+            else if (theState == State.InGame)
+            {
+
+            }
+            else if (theState == State.Pause)
+            {
+                DeactivateAll();
+                //pauseMenu.SetActive(true);
+            }
+            else if (theState == State.Credits)
+            {
+
+            }
+            else
+            {
+                Debug.Log("Unrecognized State: " + value.ToString());
+            }
+        }
+    }
+
+    private void DeactivateAll()
+    {
+        foreach (GameObject g in screens)
+        {
+            g.SetActive(false);
+        }
+    }
+
+    // Use this for initialization
+    void Awake ()
+    {
+        foreach(Text f in FindObjectsOfType<Text>())
+        {
+            f.font = ourFont; // Not working for some reason.  Sets all to no font, despite having a font
+        }
+
+        screens = GameObject.FindGameObjectsWithTag("Screen");
+
+        if (SceneManager.GetActiveScene().name.CompareTo("MainMenu") == 0)
+        {
+            Debug.Log("At the Main Menu");
+            TheState = State.PressStart;
+
+            foreach (GameObject g in screens)
+            {
+                if (g.name.CompareTo("PressStart") == 0)
+                {
+                    pressStartMenu = g;
+                    g.SetActive(true);
+                }
+                else if (g.name.CompareTo("MainMenu") == 0)
+                {
+                    mainMenu = g;
+                    g.SetActive(false);
+                }
+                else if (g.name.CompareTo("PauseMenu") == 0)
+                {
+                    pauseMenu = g;
+                    g.SetActive(false);
+                }
+                else
+                {
+                    g.SetActive(false);
+                }
+            }
+        }
+        else if (SceneManager.GetActiveScene().name.CompareTo("FirstLevel") == 0)
+        {
+            Debug.Log("InGame");
+            TheState = State.InGame;
+
+            foreach (GameObject g in screens)
+            {
+                g.SetActive(false);
+            }
+            
+        }
+
+        instance = this;
+    }
+
+    // Update is called once per frame
+    void Update ()
+    {
+        if (TheState == State.PressStart && Input.anyKey) TheState = State.MainMenu;
+
+        if (TheState == State.InGame && Input.GetKeyDown(KeyCode.Escape) || Input.GetKeyDown(KeyCode.P)) TheState = State.Pause;
+        else if (TheState == State.Pause && Input.GetKeyDown(KeyCode.Escape) || Input.GetKeyDown(KeyCode.P)) TheState = State.InGame;
+
+    }
+
+    public void LoadLevel(string name)
+    {
+        SceneManager.LoadScene(name);
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
