50dcd1dd8a071f9308adc1373950a013b8ab2e2e
dgreyleman <dgreyleman@aol.com>
2017-01-21 10:52:52 -0700

    Selectables
    Changes for Selectable objects

---
+++ GGJ/Test/Assets/Scripts/Clicker.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Clicker : MonoBehaviour
+{
+    private GameObject lastSelected;
+	// Use this for initialization
+	void Start ()
+    {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit))
+        {
+            if (hit.collider.gameObject != lastSelected)
+            {
+                if (lastSelected != null && lastSelected.GetComponent<Selectable>() != null && lastSelected.GetComponent<Selectable>().GetSelected())
+                {
+                    lastSelected.GetComponent<Selectable>().Deselect();
+                }
+            }
+            lastSelected = hit.collider.gameObject;
+            if (lastSelected.GetComponent<Selectable>() != null)
+            {
+                if (!lastSelected.GetComponent<Selectable>().GetSelected())
+                {
+                    lastSelected.GetComponent<Selectable>().Select();
+                }
+            }
+        }
+    }
+}
---
+++ GGJ/Test/Assets/Scripts/Selectable.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Selectable : MonoBehaviour
+{
+    public GameObject highlight;
+    public int defaultLayer = 0;
+    private bool selected = false;
+
+    public void Select()
+    {
+        highlight.SetActive(true);
+        gameObject.layer = 11;
+        selected = true;
+    }
+
+    public void Deselect()
+    {
+        highlight.SetActive(false);
+        gameObject.layer = defaultLayer;
+        selected = false;
+    }
+
+    public bool GetSelected()
+    {
+        return selected;
+    }
+}
