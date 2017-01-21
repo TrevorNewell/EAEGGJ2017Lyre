9169c4362dec0d87072763c20b137054d049a303
TrevorNewell <u0774750@utah.edu>
2017-01-20 22:00:05 -0700

    Initial Commit.  Whitebox Level and Tests.

---
+++ GGJ/Test/Assets/Scripts/ColorListener.cs
@@ -0,0 +1,93 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ColorListener : MonoBehaviour
+{
+    public bool isActive;
+
+    public int colorValue;    // For simplicity, 1 corresponds to Red, 2 corresponds to Blue
+
+
+    public Material defaultMaterial;
+    public Material activeMaterial;
+
+    public bool disableColliderIfInactive;
+    public Collider theCollider;
+    private Material[] defaultMaterialArray;
+    private Material[] activeMaterialArray;
+
+    private float overlap; // How long after this is disabled will it stay active?
+    private float currentRange;
+
+    private LightEmUp theScript;
+
+    private bool countdownActive = false;
+    private float temp = 0;
+
+	// Use this for initialization
+	void Start ()
+    {
+        theScript = GameObject.FindGameObjectWithTag("Player").GetComponent<LightEmUp>();
+        defaultMaterial = gameObject.GetComponent<MeshRenderer>().materials[0];
+        currentRange = 0;
+
+        overlap = theScript.overlapTime;
+
+        defaultMaterialArray = new Material[1];
+        defaultMaterialArray[0] = defaultMaterial;
+
+        activeMaterialArray = new Material[1];
+        activeMaterialArray[0] = activeMaterial;
+
+        if (disableColliderIfInactive) theCollider.enabled = false;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        bool currentlyActive = isActive;
+
+        currentRange = theScript.currentRange;
+
+        DetermineActivity();
+
+        if (countdownActive)
+        {
+            temp += Time.deltaTime;
+
+            if (temp >= overlap)
+            {
+                countdownActive = false;
+                gameObject.GetComponent<MeshRenderer>().materials = defaultMaterialArray;
+                if (disableColliderIfInactive) theCollider.enabled = false;
+            }
+        }
+    }
+
+    public float CalculateDistance()
+    {
+        return Vector3.Distance(theScript.gameObject.transform.position, transform.position);
+    }
+
+    public void DetermineActivity()
+    {
+        if (CalculateDistance() <= currentRange && colorValue == theScript.currentColor)
+        {
+            gameObject.GetComponent<MeshRenderer>().materials = activeMaterialArray;
+
+            isActive = true;
+
+            if (disableColliderIfInactive) theCollider.enabled = true;
+        }
+        else
+        {
+            if (isActive)
+            {
+                temp = 0;
+                countdownActive = true;
+            }
+
+            isActive = false;
+        }
+    }
+}
---
+++ GGJ/Test/Assets/Scripts/LightEmUp.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LightEmUp : MonoBehaviour
+{
+    public AudioClip[] tracks;
+    public int currentColor = 0;    // For simplicity, 1 corresponds to Red, 2 corresponds to Blue
+    public float maxRange = 100;
+    public float expansionRate = 0.1f;
+    public float overlapTime = 2.0f; // How long after being disabled, is an object still lit up?
+    public float currentRange = 0.0f;
+
+    private AudioSource theAudioSource;
+
+	// Use this for initialization
+	void Start ()
+    {
+        theAudioSource = GetComponent<AudioSource>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (currentRange < maxRange)
+        {
+            currentRange += expansionRate;
+        }
+
+        if (currentRange > maxRange)
+        {
+            currentRange = maxRange;
+        }
+
+	    if (Input.GetKeyDown(KeyCode.Alpha1) && currentColor != 1)
+        {
+            Activate(1);
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha2) && currentColor != 2)
+        {
+            Activate(2);
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha3) && currentColor != 3)
+        {
+            Activate(3);
+        }
+
+        //Debug.Log("Color: " + currentColor + " Range: " + currentRange);
+	}
+
+    public void Activate(int color)
+    {
+        if (currentColor != color)
+        {
+            currentRange = 0;
+            currentColor = color;
+
+            if (tracks.Length >= 3) // I assume this has 3 tracks for now.  1 for the first string, 2 for the second string, 3 for no string being played.
+            {
+                theAudioSource.Stop();
+                theAudioSource.clip = tracks[currentColor - 1];
+                theAudioSource.Play();
+            }
+        }
+    }
+
+}
